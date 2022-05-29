b0cdd76709d72ff5c1c3330de94fbb518f8d717e
Bruno Hubner <brunohubnerdev@gmail.com>
2022-05-28 21:56:10 -0300

    refactor: update template email

--- src/utils/createHTMLEmailMessage.ts
+++ src/utils/createHTMLEmailMessage.ts
@@ -1,12 +1,40 @@
 export function createHTMLEmailMessage(data: string): string {
     return `
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+    </head>
+    <body>
         <style>
-            .message-content { font-family: Arial, Helvetica, sans-serif; max-width:
-            600px; font-size: 18px; line-height: 24px; }
+            * {
+                box-sizing: border-box;
+                padding: 0;
+                margin: 0;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+            html,
+            body {
+                width: 100%;
+                height: 100%;
+                background-color: #222;
+            }
+            #message-content {
+                max-width: 40rem;
+                height: 100%;
+                color: #eee;
+                font-size: 1.25rem;
+                line-height: 2rem;
+                padding: 1.5rem;
+            }
         </style>
-
-        <div class="message-content">
+        <div id="message-content">
             ${data}
         </div>
+    </body>
+    </html>
     `
 }
