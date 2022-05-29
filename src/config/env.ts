1219955e9e017ea3b1f1621999d3ba8d81b2ea17
Bruno Hubner <brunohubnerdev@gmail.com>
2022-05-29 10:30:50 -0300

    fix: remove unused env vars

--- src/config/env.ts
+++ src/config/env.ts
@@ -9,6 +9,5 @@ export const env = {
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || "",
     MAIL_PROVIDER: process.env.MAIL_PROVIDER || "ethereal",
     NODE_ENV: process.env.NODE_ENV || "development",
-    IP_INFO_PROVIDER: process.env.IP_INFO_PROVIDER || "mock",
     CORS_CLOSED: process.env.CORS_CLOSED ? true : false
 }
